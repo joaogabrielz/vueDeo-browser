54502a53be0126f80eef718d4882d63fd9f7ee1b
Joao <zacarias1021@gmail.com>
2022-09-11 15:38:27 -0400

    first commit

---
+++ src/components/VideoList.vue
@@ -0,0 +1,37 @@
+<template>
+		<ul class="list-group col-md-4">
+			<VideoListItem
+				v-for="video in videos" 
+				:key="video.etag"
+				v-bind:video="video"
+				@videoSelect="onVideoSelect"
+			>
+			</VideoListItem>
+		</ul>
+</template>
+
+<script>
+import VideoListItem from './VideoListItem.vue';
+
+	export default{
+		name: 'VideoList',
+		components:{
+			VideoListItem
+		},
+		props: {
+			videos:{
+				type: Array,
+				required: true
+			},
+		},
+		methods:{
+			onVideoSelect(video){
+				this.$emit('videoSelect', video)
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/VideoListItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+		<li class="list-group-item media" @click="onVideoSelect">
+			<img class="mr-3" :src="thumbnailUrl" alt="Thumbnail of video"/>
+			<div class="media-body titleP">
+				{{ video.snippet.title }}
+			</div>
+		</li>
+	</div>
+</template>
+
+<script>
+	export default{
+		name: 'VideoListItem',
+		props: ['video'],
+		computed:{ //format data
+			thumbnailUrl(){
+				return this.video.snippet.thumbnails.default.url;
+			}
+		},
+		methods:{
+			onVideoSelect(){
+				this.$emit('videoSelect', this.video);
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	li{
+		display: flex;
+		cursor: pointer;
+	}
+	li:hover{
+		background-color: #eee ;
+	}
+	.titleP{
+		margin-left:1rem;
+	}
+
+</style>
